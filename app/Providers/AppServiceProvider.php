d66a2fc52170afb5a262cc70a8106344bbd9b2d2
ofismedia <31524330+BernaGN@users.noreply.github.com>
2022-04-07 18:51:18 -0500

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Paginator::useBootstrap();
+        Model::preventLazyLoading(!$this->app->isProduction());
+        Password::defaults(function () {
+            return Password::min(8)
+                ->mixedCase()
+                ->letters()
+                ->numbers()
+                ->symbols()
+                ->uncompromised();
+        });
+    }
+}
